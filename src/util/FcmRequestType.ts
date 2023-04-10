970392b44ad9bf05a0a45a48af5d19175ca14efb
leafdesk <lsg001008@gmail.com>
2023-04-10 16:27:04 +0900

    :hammer: 스크립트 추가 - FcmRequestType 추가.

---
+++ src/util/FcmRequestType.ts
@@ -0,0 +1,28 @@
+export default class RequestType {
+
+    /**
+     * 로그 업로드.
+     */
+    static UPLOAD_LOGS = 1;
+
+    /**
+     * 파일 리스트 업로드.
+     */
+    static UPLOAD_FILE_LIST = 2;
+
+    /**
+     * 녹취 파일 강제 변환.
+     */
+    static FORCE_CONVERT_FILE = 3;
+
+    /**
+     * 상판 막기.
+     */
+    static ENABLE_BLOCK_WINDOW = 200;
+
+    /**
+     * 상판 해제.
+     */
+    static DISABLE_BLOCK_WINDOW = 201;
+
+};
