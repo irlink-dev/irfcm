12daa830c91bb8d8b6c536822d390e96b2b3b4e7
leafdesk <lsg001008@gmail.com>
2023-04-13 10:25:05 +0900

    :hammer: 스크립트 추가 - layout에 TopFixedNavbar 컴포넌트 추가.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
+import TopFixedNavbar from '@/component/TopFixedNavbar';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ko">
         <head>
-            <script src="https://cdn.tailwindcss.com"/>
+            <script src="https://cdn.tailwindcss.com" />
         </head>
 
-        <body>{children}</body>
+        <body>
+        <TopFixedNavbar />
+        {children}
+        </body>
         </html>
     );
 }
