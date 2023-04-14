c64c9cdcc7e8ca1120faced13f9b7996550b42f4
leafdesk <lsg001008@gmail.com>
2023-04-14 16:33:21 +0900

    :lipstick: 스타일 수정 - HomePage body 스타일 추가. - TODO 주석 추가.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import GlobalStyle from '@/style/GlobalStyle';
+
 export default function HomePage() {
     return (
         <>
-            <div>IRFCM 홈.</div>
+            <div className={GlobalStyle.CONTAINER}>IRFCM 홈.</div>
         </>
     );
 }
+
+/**
+ * TODO 1. 단일 로그 요청. 로그 파일 다운로드 링크 제공.
+ * TODO 2. 법인폰 리스트. 사용 중 여부 체크. 단일 로그 요청 기능과 통합.
+ * TODO 3. 주기적 로그 요청 자동화. 어제 것 분석. 앱 재시작, 통화 횟수 등 정보 자동 분석. 대시보드.
+ */
