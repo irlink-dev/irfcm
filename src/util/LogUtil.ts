e99e844f482556c28f1c8842bd052a2c9130b8d4
leafdesk <lsg001008@gmail.com>
2023-04-18 10:26:29 +0900

    :hammer: 스크립트 추가 - LogUtil에 exception, printErrorLog 함수 추가.

--- src/util/LogUtil.ts
+++ src/util/LogUtil.ts
@@ -10,10 +10,24 @@ export default class LogUtil {
     }
 
     /**
+     * 예외(에러) 로그.
+     */
+    static exception(tag: string, error: Error) {
+        this.printErrorLog(tag, error);
+    }
+
+    /**
      * 지정된 형식에 맞춰 콘솔 로그 출력.
      */
     static printLog(tag: string, message: string) {
         console.log(`[${tag}] ${message}`);
+    }
+
+    /**
+     * 콘솔 에러 로그 출력.
+     */
+    static printErrorLog(tag: string, error: Error) {
+        console.error(`[${tag}] ${error}`);
     }
 
 }
