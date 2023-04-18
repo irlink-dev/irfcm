2c974900ad5795131cb83bb8f2ce20470e98913f
leafdesk <lsg001008@gmail.com>
2023-04-18 15:45:05 +0900

    :hammer: 스크립트 추가 - FirebaseUtil에 getFileDownloadLinks, getLogDownloadLinks 메소드 추가.

--- src/util/FirebaseUtil.ts
+++ src/util/FirebaseUtil.ts
@@ -1,9 +1,12 @@
 import firebase from 'firebase/compat/app';
 import LogUtil from '@/util/LogUtil';
+import FormatUtil from '@/util/FormatUtil';
 
 export default class FirebaseUtil {
 
     TAG: string = 'FirebaseUtil';
+
+    formatUtil = new FormatUtil();
 
     /**
      * 파이어베이스 초기화.
@@ -18,6 +21,50 @@ export default class FirebaseUtil {
         }
         return firebase;
     };
+
+    /**
+     * 파일 다운로드 링크 받기.
+     */
+    async getFileDownloadLinks(
+        filenames: Array<string>,
+        bucket: firebase.storage.Reference
+    ) {
+        const urls: Array<string> = [];
+
+        for (const filename of filenames) {
+            const fileRef = bucket.child(filename);
+            const url = await fileRef.getDownloadURL();
+            urls.push(url);
+        }
+        return urls;
+    }
+
+    /**
+     * 로그 다운로드 링크 받기.
+     */
+    async getLogDownloadLinks(
+        phoneNumber: string,
+        date: string,
+        bucket: firebase.storage.Reference
+    ) {
+        const phoneNumberWithHyphen = this.formatUtil.formatPhoneNumberWithHyphen(phoneNumber);
+        const dateWithoutHyphen = date.replace(/-/g, '');
+
+        const filenames = [
+            `log/${phoneNumberWithHyphen}/${date}/log_${dateWithoutHyphen}.txt`,                    // 앱 로그.
+            `log/${phoneNumberWithHyphen}/${date}/IRRecorderLog_${dateWithoutHyphen}.txt`,          // 레코더 로그.
+        ];
+        try {
+            const urls = await this.getFileDownloadLinks(filenames, bucket);
+            LogUtil.d(this.TAG, `getLogDownloadLinks. length: ${urls.length}`);
+            return urls;
+
+        } catch (error) {
+            LogUtil.exception(this.TAG, error);
+            LogUtil.d(this.TAG, `getLogDownloadLinks. return empty array.`);
+            return [];
+        }
+    }
 
 }
 
