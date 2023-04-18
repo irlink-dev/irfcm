94f35eebbdf7c35cbb76d317151c2f6a8ea38af1
leafdesk <lsg001008@gmail.com>
2023-04-18 15:43:05 +0900

    :hammer: 코드 수정 - FcmRequestForm에 bucket, urls, message 상태 관리 추가. - priority select 주석 처리. 'high'로 고정. - FCM 요청에 대한 응답이 오면, 자동으로 로그 가져오기.

--- src/component/FcmRequestForm.tsx
+++ src/component/FcmRequestForm.tsx
@@ -3,11 +3,13 @@
 import React, { useEffect, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { get, getDatabase, ref } from 'firebase/database';
+import 'firebase/compat/storage';
 import GlobalStyle from '@/style/GlobalStyle';
 import FirebaseUtil from '@/util/FirebaseUtil';
 import LogUtil from '@/util/LogUtil';
 import FcmRequestType from '@/util/FcmRequestType';
 import SendFcmUseCase from '@/domain/SendFcmUseCase';
+import FormatUtil from '@/util/FormatUtil';
 
 export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Props) {
 
@@ -16,13 +18,6 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
     const firebaseUtil = new FirebaseUtil();
     const sendFcmUseCase = new SendFcmUseCase();
 
-
-    /**
-     * 중요도.
-     */
-    const priority = {
-        HIGH: 'high'
-    };
 
     /**
      * 녹취 포함 여부.
@@ -40,7 +35,7 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
         date: string,
         requestType: number,
         isIncludeRecord: boolean,
-        priority: string,
+        // priority: string,
     }
 
     /**
@@ -51,11 +46,29 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
         date: '2023-04-11',
         requestType: FcmRequestType.UPLOAD_LOGS,
         isIncludeRecord: false,
-        priority: 'high'
+        // priority: 'high'
     });
 
+    /**
+     * 파이어베이스 스토리지 버킷.
+     */
+    const [bucket, setBucket] = useState<any>();
+
+    /**
+     * 스토리지 파일 다운로드 링크.
+     */
+    const [urls, setUrls] = useState<Array<string>>([]);
+
+    /**
+     * 사용자 알림 메시지.
+     */
+    const [message, setMessage] = useState<string>('');
+
+
     useEffect(() => {
-        firebaseUtil.initFirebaseApp(firebaseConfig);
+        const app = firebaseUtil.initFirebaseApp(firebaseConfig);
+        const bucketName = firebaseConfig?.storageBucket;
+        setBucket(() => app.storage().refFromURL(`gs://${bucketName}`));
     }, []);
 
     /**
@@ -75,7 +88,7 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
             date,                       // 날짜.
             requestType,                // 요청 타입.
             isIncludeRecord,            // 녹취 포함 여부.
-            priority                    // 중요도.
+            // priority                    // 중요도.
         } = data;                       // 사용자 입력 데이터.
 
         getFirebaseToken(phoneNumber)
@@ -88,12 +101,22 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
                     date: date,
                     requestType: requestType,
                     isIncludeRecord: isIncludeRecord,
-                    priority: priority
+                    priority: 'high'
                 };
                 sendFcmUseCase.request(request, (response) => {
                     response.task({
-                        onSuccess: () => LogUtil.d(TAG, response.message!),
-                        onFail: () => LogUtil.d(TAG, response.message!)
+                        onSuccess: () => {
+                            LogUtil.d(TAG, response.message!);
+                            setMessage(() => response.message!);
+                            setTimeout(() => {
+                                firebaseUtil.getLogDownloadLinks(phoneNumber, date, bucket)
+                                    .then(urls => setUrls(urls));
+                            }, 3000);   // 로그가 올라올 때까지 대기.
+                        },
+                        onFail: () => {
+                            LogUtil.d(TAG, response.message!);
+                            setMessage(() => response.message!);
+                        }
                     });
                 });
             })
@@ -123,7 +146,7 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
     return (
         <form className={GlobalStyle.CONTAINER}
               onSubmit={handleSubmit(onValid, onInvalid)}>
-            <div className="grid gap-6 mb-6 md:grid-cols-3">
+            <div className="grid gap-6 mb-6 md:grid-cols-4">
                 <div>
                     <label htmlFor="phone_number"
                            className={GlobalStyle.LABEL}>법인폰 번호</label>
@@ -182,47 +205,91 @@ export default function FcmRequestForm({ authorizationKey, firebaseConfig }: Pro
                         <option value={isIncludeRecord.TRUE}>true</option>
                     </select>
                 </div>
-                <div>
-                    <label htmlFor="priority"
-                           className={GlobalStyle.LABEL}>중요도</label>
-                    <select id="priority"
-                            className={GlobalStyle.INPUT}
-                            {...register('priority')}
-                            defaultValue={value.priority}
-                            onChange={event => setValue(prevState => ({
-                                ...prevState,
-                                priority: event.target.value,
-                            }))}>
-                        <option value={priority.HIGH}>높음</option>
-                    </select>
-                </div>
+                {/*<div>*/}
+                {/*    <label htmlFor="priority"*/}
+                {/*           className={GlobalStyle.LABEL}>중요도</label>*/}
+                {/*    <select id="priority"*/}
+                {/*            className={GlobalStyle.INPUT}*/}
+                {/*            {...register('priority')}*/}
+                {/*            defaultValue={value.priority}*/}
+                {/*            onChange={event => setValue(prevState => ({*/}
+                {/*                ...prevState,*/}
+                {/*                priority: event.target.value,*/}
+                {/*            }))}>*/}
+                {/*        <option value={priority.HIGH}>높음</option>*/}
+                {/*    </select>*/}
+                {/*</div>*/}
             </div>
 
-            <div className="inline-flex rounded-md shadow-sm" role="group">
-                <button type="button"
-                        className="px-4 py-2 text-sm font-medium text-gray-400 bg-white border border-gray-200 rounded-l-lg
+            <div className="flex">
+                <div className="inline-flex rounded-md shadow-sm mb-4 mr-4" role="group">
+                    <button type="button"
+                            className="px-4 py-2 text-sm font-medium text-gray-400 bg-white border border-gray-200 rounded-l-lg
                         hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-                        onClick={() => {
-                            console.log(watch());
-                            // console.log(firebaseConfig);
-                        }}>
-                    테스트 버튼
-                </button>
-                {/*<button type="button"*/}
-                {/*        className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200
+                            onClick={() => {
+                                console.log(watch());
+                                // console.log(firebaseConfig);
+                            }}>
+                        테스트 버튼
+                    </button>
+                    {/*<button type="button"*/}
+                    {/*        className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200
                            hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">*/}
-                {/*    Settings*/}
-                {/*</button>*/}
-                <button type="submit"
-                        className="px-4 py-2 text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-r-md
+                    {/*    Settings*/}
+                    {/*</button>*/}
+                    <button type="submit"
+                            className="px-4 py-2 text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-r-md
                         hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-                    FCM 요청
-                </button>
+                        FCM 요청
+                    </button>
+                </div>
+
+                {message.length == 0 ? '' : message.length < 16 ? (
+                    <span className='text-green-700'>FCM 전송 성공.</span>
+                ) : (
+                    <span className='text-red-700'>FCM 전송 실패.</span>
+                )}
+            </div>
+
+
+            <div className="bg-white py-5">
+                {/*<dt className="text-sm font-medium text-gray-500">저장소 파일</dt>*/}
+                {urls.length > 0 ? (
+                    <dd className="mt-1 text-sm text-gray-900">
+                        <ul
+                            role="list"
+                            className="divide-y divide-gray-200 rounded-md border border-gray-200"
+                        >
+                            {urls.map(url => (
+                                <FileListRow key={url} url={url} />
+                            ))}
+                        </ul>
+                    </dd>
+                ) : null}
             </div>
 
         </form>
     );
 
+}
+
+function FileListRow({ url }: any) {
+
+    const formatUtil = new FormatUtil();
+    const fileName = formatUtil.extractFileNameFromUrl(url);
+
+    return (
+        <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+            <div className="flex w-0 flex-1 items-center">
+                <span className="ml-2 w-0 flex-1 truncate">{fileName}</span>
+            </div>
+            <div className="ml-4 flex-shrink-0">
+                <a href={url}
+                   className="font-medium text-indigo-600 hover:text-indigo-500">다운로드
+                </a>
+            </div>
+        </li>
+    );
 }
 
 interface Props {
