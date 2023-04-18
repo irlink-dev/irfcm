9984aacdebce8459be73c9860009c6e5ac6fac8c
leafdesk <lsg001008@gmail.com>
2023-04-18 15:43:43 +0900

    :hammer: 코드 수정 - TopFixedNavbar 사용하지 않는 부분 주석 처리. - 아이알링크 로고 추가.

--- src/component/TopFixedNavbar.tsx
+++ src/component/TopFixedNavbar.tsx
@@ -44,11 +44,10 @@ export default function TopFixedNavbar() {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-                <a className="flex items-center" onClick={() => router.push('/')}>
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                    <span
-                        className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
+                <button className="flex items-center" onClick={() => router.push('/')}>
+                    <img src="https://cdn.imweb.me/thumbnail/20200406/37563d3a84b79.png" className="h-10"
+                         alt="Flowbite Logo" />
+                </button>
                 <div className="flex items-center md:order-2">
                     <a href="#"
                        className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
@@ -70,18 +69,18 @@ export default function TopFixedNavbar() {
                 <div id="mega-menu-icons"
                      className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-                        <li onClick={() => router.push('/')}>
-                            <a className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-                               aria-current="page">Home</a>
-                        </li>
+                        {/*<li onClick={() => router.push('/')}>*/}
+                        {/*    <a className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"*/}
+                        {/*       aria-current="page">Home</a>*/}
+                        {/*</li>*/}
                         <li>
                             <button id="mega-menu-icons-dropdown-button"
                                     data-dropdown-toggle="mega-menu-icons-dropdown"
-                                    onClick={handleDropdown}            // 클릭 시 드롭다운 상태 반전.
+                                    // onClick={handleDropdown}            // 클릭 시 드롭다운 상태 반전.
                                     onMouseEnter={openDropdown}         // 트리거 위에 마우스를 올리면 메뉴 표시.
                                     onMouseLeave={closeDropdown}        // 트리거 밖으로 마우스가 나가면 메뉴 숨김.
                                     className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                                Company
+                                고객사 목록
                                 <svg aria-hidden="true" className="w-5 h-5 ml-1" fill="currentColor"
                                      viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd"
@@ -270,10 +269,10 @@ export default function TopFixedNavbar() {
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <a href="#"
-                               className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</a>
-                        </li>
+                        {/*<li>*/}
+                        {/*    <a href="#"*/}
+                        {/*       className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</a>*/}
+                        {/*</li>*/}
                     </ul>
                 </div>
             </div>
