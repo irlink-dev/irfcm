2f37ab2bb2a6146bcf8a38e3a45c8da9e72719e9
leafdesk <lsg001008@gmail.com>
2023-05-02 13:36:53 +0900

    :hammer: 코드 수정 - clientKey 타입 명시. 객체에 동적으로 접근 가능. - getClientKeyFromPathname, getAuthorizationKey 메소드 추가.

--- src/manager/FirebaseManager.ts
+++ src/manager/FirebaseManager.ts
@@ -1,64 +1,58 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/storage';
-import FirebaseUtil from '@/util/FirebaseUtil';
-import * as process from 'process';
-import IrFirebaseConfig from '@/util/IrFirebaseConfig';
+import 'firebase/compat/storage'
+import * as process from 'process'
+import IrFirebaseConfig from '@/util/IrFirebaseConfig'
 
 
 export default class FirebaseManager {
 
-    static TAG: string = 'FirebaseManager';
+    irFirebaseConfig = new IrFirebaseConfig()
 
-    firebaseApp: any;
-    firebaseStorageBucket: any;
-    authorizationKey: string = '';
-
-    firebaseUtil = new FirebaseUtil();
-    irFirebaseConfig = new IrFirebaseConfig();
-
-    initFirebaseStorageBucket() {
-
-    }
-
-    public clientKey = {
+    /**
+     * 고객사 키.
+     */
+    public clientKey: Record<string, string> = {
         CHUBB: 'CHUBB',
         DB_LIFE: 'DB_LIFE',
         HANA: 'HANA',
         KB_WIRELESS: 'KB_WIRELESS',
         LINA: 'LINA',
         SHINHAN_CARD: 'SHINHAN_CARD',
-        ZILINK: 'ZILINK'
-    };
-
-    setAuthorizationKey(key: string) {
-        if (key == '') {
-            this.authorizationKey = '';
-
-        } else if (key == this.clientKey.CHUBB) {
-            this.authorizationKey = process.env.CHUBB_AUTHORIZATION_KEY!;
-
-        } else if (key == this.clientKey.DB_LIFE) {
-            this.authorizationKey = process.env.DBLIFE_AUTHORIZATION_KEY!;
-
-        } else if (key == this.clientKey.HANA) {
-            this.authorizationKey = process.env.HANA_AUTHORIZATION_KEY!;
-
-        } else if (key == this.clientKey.KB_WIRELESS) {
-            this.authorizationKey = process.env.KB_AUTHORIZATION_KEY!;
-
-        } else if (key == this.clientKey.LINA) {
-            this.authorizationKey = process.env.LINA_AUTHORIZATION_KEY!;
-
-        } else if (key == this.clientKey.SHINHAN_CARD) {
-            this.authorizationKey = process.env.SHINHAN_AUTHORIZATION_KEY!;
-
-        } else if (key == this.clientKey.ZILINK) {
-            this.authorizationKey = process.env.ZILINK_AUTHORIZATION_KEY!;
-
-        } else {
-            this.authorizationKey = '';
-        }
+        ZILINK: 'ZILINK',
     }
 
+    /**
+     * 경로를 고객사 키로 변환.
+     */
+    getClientKeyFromPathname(pathname: 'chubb' | 'dblife' | 'hana' | 'kb' | 'lina' | 'shinhan' | 'zilink') {
+        const clientKey = {
+            'chubb': this.clientKey.CHUBB,
+            'dblife': this.clientKey.DB_LIFE,
+            'hana': this.clientKey.HANA,
+            'kb': this.clientKey.KB_WIRELESS,
+            'lina': this.clientKey.LINA,
+            'shinhan': this.clientKey.SHINHAN_CARD,
+            'zilink': this.clientKey.ZILINK
+        }
+        return clientKey[pathname]
+    }
+
+    /**
+     * 인증 키 반환.
+     */
+    getAuthorizationKey(clientKey: string) {
+        type AuthorizationKey = {
+            [key: string]: string
+        }
+        const authorizationKey: AuthorizationKey = {
+            'CHUBB': process.env.CHUBB_AUTHORIZATION_KEY!,
+            'DB_LIFE': process.env.DBLIFE_AUTHORIZATION_KEY!,
+            'HANA': process.env.HANA_AUTHORIZATION_KEY!,
+            'KB_WIRELESS': process.env.KB_AUTHORIZATION_KEY!,
+            'LINA': process.env.LINA_AUTHORIZATION_KEY!,
+            'SHINHAN_CARD': process.env.SHINHAN_AUTHORIZATION_KEY!,
+            'ZILINK': process.env.ZILINK_AUTHORIZATION_KEY!
+        }
+        return authorizationKey[clientKey]
+    }
 
 }
