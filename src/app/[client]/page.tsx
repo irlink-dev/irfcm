9632f80ea8245ac014d1871572d1497ae96be3f5
leafdesk <lsg001008@gmail.com>
2023-05-02 13:35:45 +0900

    :recycle: 코드 리팩토링 - 고객사 페이지 통합 w/ dynamic routes.

---
+++ src/app/[client]/page.tsx
@@ -0,0 +1,36 @@
+import GlobalStyle from '@/style/GlobalStyle'
+import IrFirebaseConfig from '@/util/IrFirebaseConfig'
+import FirebaseManager from '@/manager/FirebaseManager'
+import FcmRequestForm from '@/component/FcmRequestForm'
+
+export default function ClientPage({ params }: any) {
+
+    const irFirebaseConfig = new IrFirebaseConfig()
+    const firebaseManager = new FirebaseManager()
+
+    const clientKey = firebaseManager.getClientKeyFromPathname(params.client)
+    const firebaseConfig = irFirebaseConfig.getFirebaseConfig(clientKey)
+    const authorizationKey = firebaseManager.getAuthorizationKey(clientKey)
+
+    type ClientName = {
+        [key: string]: string
+    }
+    const clientName: ClientName = {
+        CHUBB: '처브 CDM',
+        DB_LIFE: 'DB 생명',
+        HANA: '하나손해보험',
+        KB_WIRELESS: 'KB 손해보험',
+        LINA: '라이나 생명',
+        SHINHAN_CARD: '신한카드',
+        ZILINK: '지링크',
+    }
+
+    return (
+        <>
+            <h1 className={GlobalStyle.CLIENT_NAME}>{clientName[clientKey]}</h1>
+
+            <FcmRequestForm authorizationKey={authorizationKey!}
+                            firebaseConfig={firebaseConfig!} />
+        </>
+    )
+}
