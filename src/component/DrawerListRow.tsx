5ca8b5d663fea50ece973dbe52b3fea29cf4c7f7
leafdesk <lsg001008@gmail.com>
2023-05-24 15:47:10 +0900

    :hammer: 코드 수정 - DrawerListRow 컴포넌트에서 선택된 ListItemButton은 다른 스타일로 표시.

--- src/component/DrawerListRow.tsx
+++ src/component/DrawerListRow.tsx
@@ -1,17 +1,20 @@
+'use client'
+
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { ListItemButton, ListItemText, ListItemAvatar } from '@mui/material'
 
 interface DrawerListRowProps {
-    name: string,
-    desc: string,
-    route: string,
+    name: string
+    desc: string
+    route: string
     image: string
 }
 
 const DrawerListRow = ({ name, desc, route, image }: DrawerListRowProps) => {
     const router = useRouter()
-    return <ListItemButton divider onClick={() => router.push(route)}>
+    const pathname = usePathname()
+    return <ListItemButton divider selected={pathname === route} onClick={() => router.push(route)}>
         <ListItemAvatar sx={{ marginRight: 2 }}>
             <Image src={image} alt={name} width={64} height={64} priority={true} />
         </ListItemAvatar>
