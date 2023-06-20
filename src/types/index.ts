35be94962496a3c8fc498a2be18eef7e7dff431c
leafdesk <lsg001008@gmail.com>
2023-06-20 17:08:00 +0900

    :recycle: 코드 리팩토링 - FcmRequestFormProps, RequestValues type 분리.

--- src/types/index.ts
+++ src/types/index.ts
@@ -44,3 +44,44 @@ export interface FirebaseConfig {
     measurementId?: string,
 
 }
+
+/**
+ * FCM 요청 양식 Props
+ */
+export interface FcmRequestFormProps {
+
+    /**
+     * 인증 키.
+     */
+    authorizationKey: string
+
+    /**
+     * 파이어베이스 설정.
+     */
+    firebaseConfig: FirebaseConfig
+
+}
+
+export interface RequestValues {
+
+    /**
+     * 법인폰 번호.
+     */
+    phoneNumber: string
+
+    /**
+     * 날짜.
+     */
+    date: string
+
+    /**
+     * 요청 타입.
+     */
+    type: number
+
+    /**
+     * 녹취 포함 여부.
+     */
+    isIncludeRecord: boolean
+
+}
