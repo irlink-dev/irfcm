55e44a0b5bfe85e8b8584f6b406ec54cee35fae0
leafdesk <lsg001008@gmail.com>
2023-07-13 18:17:49 +0900

    :art: 코드 정렬 - 세미콜론 제거 등 코드 가독성 개선.

--- src/util/FirebaseUtil.ts
+++ src/util/FirebaseUtil.ts
@@ -1,29 +1,32 @@
-import firebase from 'firebase/compat/app';
-import FormatUtil from '@/util/FormatUtil';
-import 'firebase/compat/storage';
-import { get, getDatabase, ref } from 'firebase/database';
-import SendFcmUseCase from '@/domain/SendFcmUseCase';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/storage'
+import { get, getDatabase, ref } from 'firebase/database'
+import SendFcmUseCase from '@/domain/SendFcmUseCase'
 import FirebaseConfig from '@/types/FirebaseConfig'
+import useFormat from '@/hooks/useFormat'
+
+const {
+    toHyphenNumber
+} = useFormat()
 
 export default class FirebaseUtil {
 
-    TAG: string = 'FirebaseUtil';
+    TAG: string = 'FirebaseUtil'
 
-    formatUtil = new FormatUtil();
-    sendFcmUseCase = new SendFcmUseCase();
+    sendFcmUseCase = new SendFcmUseCase()
 
     /**
      * 파이어베이스 초기화.
      */
     initFirebaseApp(firebaseConfig: FirebaseConfig) {
-        console.log(this.TAG, `initFirebaseApp. projectId: ${firebaseConfig.projectId}`);
+        console.log(this.TAG, `initFirebaseApp. projectId: ${firebaseConfig.projectId}`)
 
         if (firebase.apps.length == 0) {
-            firebase.initializeApp(firebaseConfig);                                         // 앱이 존재하지 않으면, 앱을 초기화.
+            firebase.initializeApp(firebaseConfig)                                         // 앱이 존재하지 않으면, 앱을 초기화.
         } else {
-            firebase.app().delete().then(() => firebase.initializeApp(firebaseConfig));     // 앱이 존재하면, 삭제하고 다시 초기화.
+            firebase.app().delete().then(() => firebase.initializeApp(firebaseConfig))     // 앱이 존재하면, 삭제하고 다시 초기화.
         }
-        return firebase;
+        return firebase
     };
 
     /**
@@ -33,14 +36,14 @@ export default class FirebaseUtil {
         filenames: Array<string>,
         bucket: firebase.storage.Reference
     ) {
-        const urls: Array<string> = [];
+        const urls: Array<string> = []
 
         for (const filename of filenames) {
-            const fileRef = bucket.child(filename);
-            const url = await fileRef.getDownloadURL();
-            urls.push(url);
+            const fileRef = bucket.child(filename)
+            const url = await fileRef.getDownloadURL()
+            urls.push(url)
         }
-        return urls;
+        return urls
     }
 
     /**
@@ -51,22 +54,22 @@ export default class FirebaseUtil {
         date: string,
         bucket: firebase.storage.Reference
     ) {
-        const phoneNumberWithHyphen = this.formatUtil.formatPhoneNumberWithHyphen(phoneNumber);
-        const dateWithoutHyphen = date.replace(/-/g, '');
+        const phoneNumberWithHyphen = toHyphenNumber(phoneNumber)
+        const dateWithoutHyphen = date.replace(/-/g, '')
 
         const filenames = [
             `log/${phoneNumberWithHyphen}/${date}/log_${dateWithoutHyphen}.txt`,                    // 앱 로그.
             `log/${phoneNumberWithHyphen}/${date}/IRRecorderLog_${dateWithoutHyphen}.txt`,          // 레코더 로그.
-        ];
+        ]
         try {
-            const urls = await this.getFileDownloadLinks(filenames, bucket);
-            console.log(this.TAG, `getLogDownloadLinks. length: ${urls.length}`);
-            return urls;
+            const urls = await this.getFileDownloadLinks(filenames, bucket)
+            console.log(this.TAG, `getLogDownloadLinks. length: ${urls.length}`)
+            return urls
 
         } catch (error) {
-            console.log(this.TAG, `getLogDownloadLinks. return empty array.`);
-            console.log(this.TAG, error);
-            return [];
+            console.log(this.TAG, `getLogDownloadLinks. return empty array.`)
+            console.log(this.TAG, error)
+            return []
         }
     }
 
@@ -74,16 +77,19 @@ export default class FirebaseUtil {
      * 법인폰 토큰 전체 조회.
      */
     async getAllTokens() {
-        const database = getDatabase();
-        const snapshot = await get(ref(database, 'users'));
-        return snapshot.val();
+        // FirebaseError: Firebase: Need to provide options, when not being deployed to hosting via source. (app/no-options).
+        // TODO Firebase App 삭제 런타임 오류 발생 예상 지점.
+        const database = getDatabase()
+
+        const snapshot = await get(ref(database, 'users'))
+        return snapshot.val()
     }
 
     /**
      *  법인폰 전체에 FCM 요청.
      */
     async sendFcmToAllTokens(key: string, date: string) {
-        const tokens = await this.getAllTokens();
+        const tokens = await this.getAllTokens()
 
         for (const token in tokens) {
             const request = {
@@ -93,10 +99,10 @@ export default class FirebaseUtil {
                 isIncludeRecord: true,
                 priority: 'high',
                 authorizationKey: key
-            };
+            }
             this.sendFcmUseCase.request(request, () => {
                 /* empty */
-            });
+            })
         }
     }
 
@@ -104,25 +110,25 @@ export default class FirebaseUtil {
      * 로그 폴더 내 모든 파일 가져오기.
      */
     async getLogsInFolder(phoneNumber: string, date: string) {
-        const phoneNumberWithHyphen = this.formatUtil.formatPhoneNumberWithHyphen(phoneNumber);
-        const directoryPath = `log/${phoneNumberWithHyphen}/${date}`;
-        const directoryRef = firebase.storage().ref(directoryPath);
+        const phoneNumberWithHyphen = toHyphenNumber(phoneNumber)
+        const directoryPath = `log/${phoneNumberWithHyphen}/${date}`
+        const directoryRef = firebase.storage().ref(directoryPath)
 
-        const { items } = await directoryRef.listAll();
+        const { items } = await directoryRef.listAll()
         const urls = await Promise.all(
             items.map(item => item.getDownloadURL())
-        );
-        console.log(this.TAG, `getLogsInFolder. phoneNumber: ${phoneNumber}, urls: ${urls.length}`);
-        return urls;
+        )
+        console.log(this.TAG, `getLogsInFolder. phoneNumber: ${phoneNumber}, urls: ${urls.length}`)
+        return urls
     }
 
     /**
      * 법인폰 번호 리스트 가져오기.
      */
     async getPhoneNumberList(bucket: firebase.storage.Reference) {
-        const folderRef = bucket.child('log');
-        const { prefixes } = await folderRef.listAll();
-        return prefixes.map(({ name }) => name);
+        const folderRef = bucket.child('log')
+        const { prefixes } = await folderRef.listAll()
+        return prefixes.map(({ name }) => name)
     }
 }
 
