bfbf5642b3862d246a69d265ffa5f7ec1d6a778a
leafdesk <lsg001008@gmail.com>
2023-12-11 15:49:02 +0900

    add: file 유틸 및 파일 읽기(readFiles) 함수 추가

---
+++ src/utils/file.ts
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+import Logger from '@/utils/log'
+
+const TAG = 'utils/file'
+
+/**
+ * 파일 읽기.
+ */
+export const readFiles = (directoryPath: string, extension: string = '') => {
+  let files = fs
+    .readdirSync(directoryPath)
+    .map((file) => path.join(directoryPath, file))
+
+  if (extension !== '') {
+    const extensionPattern = new RegExp(`.${extension}$`, 'i')
+    files = files.filter((file) => extensionPattern.test(file))
+  }
+  Logger.log(TAG, `readFiles. length: ${files.length}`)
+  return files
+}
