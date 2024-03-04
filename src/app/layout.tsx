d7092da6ff87a45831c786db9de29fb093a6f6d6
leafdesk <lsg001008@gmail.com>
2024-03-04 14:31:17 +0900

    design: 전체 폰트를 `Noto Sans KR` 구글 웹 폰트로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import '@/styles/globals.css'
 import Dashboard from '@/components/dashboard'
 import { SnackbarProvider } from 'notistack'
 import { LoadingProvider } from '@/contexts/loading-context'
@@ -12,6 +13,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <title>IRFCM :: IRLINK Firebase Cloud Messaging Service</title>
         <link rel="icon" href="/icons/postman.svg" />
         <meta charSet="utf-8" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        ></link>
       </head>
 
       <body>
